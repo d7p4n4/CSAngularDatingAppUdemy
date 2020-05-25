8c1b70382ff50fbd5d3316c75a43190f85dacdd6
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-25 16:50:53 +0200

    1.20200525.3

--- CSAngularDatingAppUdemy/Startup.cs
+++ CSAngularDatingAppUdemy/Startup.cs
@@ -29,6 +29,8 @@ namespace CSAngularDatingAppUdemy
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +44,8 @@ namespace CSAngularDatingAppUdemy
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
